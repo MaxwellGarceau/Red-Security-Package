b4f710d02b6adfe041fe06c211354fc111394e17
Max Garceau <maxwellgarceau@gmail.com>
2020-05-24 15:43:36 -0500

    Added combine funtion to merge plugin groups. Added compare function between plugin groups. Added HTML and CSS to display data

--- plugin-history/class-red-sp-plugin-history.php
+++ plugin-history/class-red-sp-plugin-history.php
@@ -19,6 +19,10 @@ class Plugin_History {
   }
 
   public static function admin_enqueue_scripts() {
+    /* Styles */
+    wp_enqueue_style( 'ph-style', self::get_directory_url() . '/css/ph-style.css' );
+
+    /* Scripts */
     wp_enqueue_script( 'ph-get-table-column', self::get_directory_url() . '/js/get-table-column.js', array( 'jquery' ) );
     wp_enqueue_script( 'ph-plugin-data', self::get_directory_url() . '/js/plugin-data.js', array( 'jquery' ) );
   }
@@ -71,35 +75,44 @@ class Plugin_History {
    * HTML
    */
   public static function output_admin_menu() {
-    $plugins = Plugin_History::get_plugins(); // Hardcoded for now
-    // wp_die(var_dump($plugins));
+    $current_plugins = Plugin_History::get_plugins(); // Hardcoded for now
+    // wp_die(var_dump($current_plugins));
     // $plugin_reports = self::get_options();
     // $plugin_reports = $plugin_reports['plugin_reports'];
     // $plugin_reports = array_values( $plugin_reports[0] )[0];
+// wp_die(var_dump($current_plugins));
+    $fake_old_plugins = $current_plugins;
+    $fake_old_plugins['akismet/akismet.php']['Version'] = '2.21';
+    self::combine_plugin_groups($current_plugins,$fake_old_plugins);
 
+    $output .= '<div class="ph-wrap">';
 
-    $output = '<div class="wrap">';
-      $output .= '<p>*Click column title to copy text.</p>';
-      $output .= self::get_plugin_table( $plugins );
+      $output .= '<h1>Plugin History</h1>';
 
+      $output .= '<p class="ph-notice">*Click column title to copy text</p>';
 
-      // $output .= '<hr />';
+      $output .= '<div class="ph-all-tables-container">';
 
-      // $output .= '<h2>Last saved plugin table</h2>';
+        $output .= '<div class="ph-table-container">';
 
-      // $output .= self::get_plugin_table( $plugin_reports ); // Previous plugin report
+          $output .= self::get_plugin_table( $current_plugins, $current_plugins );
 
-      $output .= self::get_save_plugins_button();
+          $output .= self::get_save_plugins_button();
 
-    $output.= '</div>';
+        $output .= '</div>';
+
+      $output .= '</div>'; // .ph-all-tables-container
+
+    $output .= '</div>'; // .ph-wrap
+
     echo $output;
   }
 
-  public static function get_plugin_table( $plugins ) {
-    $output .= '<table id="plugin-history">';
+  public static function get_plugin_table( $current_plugins, $compare_plugins = array() ) {
+    $output .= '<table id="plugin-history" class="ph-plugin-table">';
     $output .= '<thead>
         <tr>
-            <th title="Click to select column">
+            <th class="ph-plugin-name-header" title="Current Plugins">
                 Plugin Name
             </th>
             <th title="Click to select column">
@@ -107,15 +120,97 @@ class Plugin_History {
             </th>
             <th title="Click to select column">
                 Last Updated
-            </th>
-        </tr>
+            </th>';
+
+      if ( ! empty( $compare_plugins ) ) {
+        $output .= '<th class="ph-table-spacer"></th>'; // Space between last table and this one
+
+        $output .= '<th class="ph-plugin-name-header" title="Last Saved Plugins">
+            Plugin Name
+        </th>
+        <th title="Click to select column">
+            Plugin Version
+        </th>
+        <th title="Click to select column">
+            Last Updated
+        </th>';
+      }
+
+    $output .= '</tr>
     </thead>';
 
-    foreach ( $plugins as $plugin ) {
-      $output .= '<tr>';
-        $output .= '<td>' . $plugin['Name'] . '</td>';
-        $output .= '<td>' . $plugin['Version'] . '</td>';
-        $output .= '<td>' . date( 'm/d/Y', $plugin['last_updated'] ) . '</td>';
+    $combined_plugins = self::combine_plugin_groups( $current_plugins, $compare_plugins );
+    foreach ( $combined_plugins as $folder_file => $plugin ) {
+      /**
+       * Compare current and old plugin
+       */
+      $current_plugin = $plugin['current_plugin'];
+      $compare_plugin = $plugin['compare_plugin'];
+      $plugin_row_classes = array();
+      $has_current_plugin = isset( $current_plugin ) && !empty( $current_plugin );
+      $has_compare_plugin = isset( $compare_plugin ) && !empty( $compare_plugin );
+
+      /* Plugin was deleted */
+      if ( ! $has_current_plugin && $has_compare_plugin ) {
+        $plugin_row_classes[] = 'ph-deleted';
+        $plugin_row_classes[] = 'ph-tooltip';
+      }
+
+      /* Plugin was added */
+      if ( $has_current_plugin && ! $has_compare_plugin ) {
+        $plugin_row_classes[] = 'ph-added';
+        $plugin_row_classes[] = 'ph-tooltip';
+      }
+
+
+      if ( $has_current_plugin && $has_compare_plugin ) {
+
+        /* Plugin was upgraded */
+        if ( version_compare( $current_plugin['Version'], $compare_plugin['Version'], '>' ) ) {
+          $plugin_row_classes[] = 'ph-upgraded';
+          $plugin_row_classes[] = 'ph-tooltip';
+
+        /* Plugin was downgraded */
+        } else if ( version_compare( $current_plugin['Version'], $compare_plugin['Version'], '<' ) ) {
+          $plugin_row_classes[] = 'ph-downgraded';
+          $plugin_row_classes[] = 'ph-tooltip';
+        }
+
+      }
+
+      $output .= '<tr class="' . implode( ' ', $plugin_row_classes ) . '">';
+        /**
+         * Current plugins
+         */
+        if ( $has_current_plugin ) {
+          $output .= '<td>' . $current_plugin['Name'] . '</td>';
+          $output .= '<td>' . $current_plugin['Version'] . '</td>';
+          $output .= '<td>' . date( 'm/d/Y', $current_plugin['last_updated'] ) . '</td>';
+        } else {
+          /* Placeholders */
+          $output .= '<td></td>';
+          $output .= '<td></td>';
+          $output .= '<td></td>';
+        }
+
+        // if ( $has_current_plugin && $has_compare_plugin ) {
+          $output .= '<td class="ph-table-spacer"></td>'; // Space between last table and this one
+        // }
+
+        /**
+         * Last Saved plugins
+         */
+        if ( $has_compare_plugin ) {
+          $output .= '<td>' . $compare_plugin['Name'] . '</td>';
+          $output .= '<td>' . $compare_plugin['Version'] . '</td>';
+          $output .= '<td>' . date( 'm/d/Y', $compare_plugin['last_updated'] ) . '</td>';
+        } else {
+          /* Placeholders */
+          $output .= '<td></td>';
+          $output .= '<td></td>';
+          $output .= '<td></td>';
+        }
+
       $output .= '</tr>';
     }
 
@@ -138,6 +233,7 @@ class Plugin_History {
   public static function get_todays_date() {
     return date( 'D M d, Y G:i' );
   }
+
   public static function get_plugins() {
     $plugins = get_plugins(); // Start with core WP plugin info
     // wp_die(var_dump($plugins));
@@ -148,6 +244,37 @@ class Plugin_History {
     }
     return $plugins;
   }
+
+  public static function combine_plugin_groups( $current_plugins, $compare_plugins ) {
+    /* Merge plugins with same name into same array */
+    $combined_plugins = array();
+
+    // DELETE AFTER TESTING
+    $compare_plugins['old-test-plugin/old-test-plugin.php'] = array( 'Name' => 'Old Plugin (was deleted)', 'Version' => '7.5.6' ); // DELETED test
+    $current_plugins['new-test-plugin/new-test-plugin.php'] = array( 'Name' => 'New Plugin (was just added)', 'Version' => '1.2.6' ); // ADDED test
+    $compare_plugins['akismet/akismet.php']['Version'] = '1.5.2'; // DOWNGRADED test
+    $compare_plugins['hello.php']['Version'] = '100.1.2'; // UPGRADED test
+
+    foreach ( $current_plugins as $key => $current_plugin ) {
+      if ( isset( $compare_plugins[$key] ) ) {
+        $combined_plugins[$key] = array( 'current_plugin' => $current_plugin, 'compare_plugin' => $compare_plugins[$key] );
+      } else {
+        $combined_plugins[$key] = array( 'current_plugin' => $current_plugin );
+      }
+    }
+
+    /* Dump all non merged plugins instances into the new array */
+    foreach ( $compare_plugins as $key => $compare_plugin ) {
+      if ( ! isset( $combined_plugins[$key] ) ) {
+        $combined_plugins[$key] = array( 'compare_plugin' => $compare_plugin );
+      }
+    }
+
+    /* Sort by alphabetical order */
+    ksort( $combined_plugins );
+    return $combined_plugins;
+  }
+
 }
 $plugin_history = Plugin_History::get_instance();
 $plugin_history->execute_hooks();
--- plugin-history/js/get-table-column.js
+++ plugin-history/js/get-table-column.js
@@ -1,45 +1,52 @@
 (function ($) {
   // Source: http://jsfiddle.net/4BwGG/3/
   function SelectColumn(index, tableId) {
-      var columnText = '';
-      var columnSelector = '#' + tableId + ' tbody > tr > td:nth-child(' + (index + 1) + ')';
-      var cells = $(columnSelector);
+    var columnText = '';
+    var columnSelector = '#' + tableId + ' tbody > tr > td:nth-child(' + (index + 1) + ')';
+    var cells = $(columnSelector);
 
-      // clear existing selections
-      if (window.getSelection) { // all browsers, except IE before version 9
-          window.getSelection().removeAllRanges();
-      }
+    // clear existing selections
+    if (window.getSelection) { // all browsers, except IE before version 9
+      window.getSelection().removeAllRanges();
+    }
 
+    if (document.createRange) {
+      cells.each(function(i, cell) {
+        var rangeObj = document.createRange();
+        rangeObj.selectNodeContents(cell);
+        window.getSelection().addRange(rangeObj);
 
-      if (document.createRange) {
-          cells.each(function(i, cell) {
-              var rangeObj = document.createRange();
-              rangeObj.selectNodeContents(cell);
-              window.getSelection().addRange(rangeObj);
-              columnText = columnText + '\r' + rangeObj.toString();
-          });
+        if (rangeObj.toString()) {
+          columnText = columnText + '\r' + rangeObj.toString();
+        }
 
+      });
 
-      }
-      else { // Internet Explorer before version 9
-          cells.each(function(i, cell) {
-              var rangeObj = document.body.createTextRange();
-              rangeObj.moveToElementText(cell);
-              rangeObj.select();
-              columnText = columnText + '\r' + rangeObj.toString();
-          });
-      }
-
-      // navigator.clipboard.writeText(columnText);
-      prompt('Copy text.', columnText.trim());
+    }
+    return columnText.trim();
   }
+
+  // Source: https://www.30secondsofcode.org/blog/s/copy-text-to-clipboard-with-javascript
+  function copyToClipboard(str) {
+    var el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  };
 
 
   $(document).ready(function() {
-      $('#plugin-history thead th').each(function(index) {
-          $(this).click(function() {
-              SelectColumn(index, 'plugin-history');
-          });
+    $('#plugin-history thead th').each(function(index) {
+      $(this).click(function(e) {
+        var columnText = SelectColumn(index, 'plugin-history');
+        copyToClipboard(columnText);
+        alert('Text successfully copied!' + '\r\n\r\n' + columnText);
       });
+    });
   });
 })(jQuery);
